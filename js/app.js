d659b6c76ee8f7b5205345c94a0aeb58b8038272
Miguel Ledesma <103437837+Xcorpi0@users.noreply.github.com>
2023-02-22 12:58:07 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+'use strict'
+// $(document).ready(function(){
+
+//     $('body').on({'mousemove': function(e){
+//         console.clear()
+//         let clientX = e.originalEvent.clientX;
+//         let clientY = e.originalEvent.clientY;
+//         $('#cursor').css({
+//             'left' : (clientX - 40) + 'px',
+//             'top' : (clientY - 40) + 'px'
+//         })
+//     }})
+//     $('a').on({
+//         'mouseover':function(){
+//             $('#cursor').addClass('mini')
+//         },
+//         'mouseout':function(){ 
+//               $('#cursor').removeClass('mini') 
+//     },
+// })
+    
+// })
+
+let hamburguesa = document.querySelector('#hamburguer');
+let menu = document.querySelector('.ul-mobile');
+const main = document.querySelector('.main');
+
+// Variables para imagenes
+const cerrarHamburguesa = '../assets/icon-close.svg';
+const abrirHamburguesa = '../assets/icon-hamburger.svg'
+let menuHamburguesa = false;
+
+console.log(hamburguesa);
+hamburguesa.addEventListener('click', () => {
+    if(menuHamburguesa == false) {
+        menuHamburguesa = true
+        hamburguesa.src = cerrarHamburguesa
+        menu.style.top = '0rem';
+        menu.style.left ='0'
+        menu.style.opacity = '1'
+    }else {
+        menuHamburguesa = false;
+        hamburguesa.src = abrirHamburguesa;
+        menu.style.top = '-200rem';
+        menu.style.left ='80rem'
+        menu.style.opacity = '0'
+    }
+   
+})
+
+
+main.addEventListener('click', ()=> {
+    hamburguesa.src = abrirHamburguesa;
+    menuHamburguesa = false;
+    menu.style.top = '-200rem';
+    menu.style.left ='80rem'
+    menu.style.opacity = '0'
+})
